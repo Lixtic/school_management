6daa754c413801c00cacc14126418bf38506d2f6
Ray Gyapanin <72960660+Lixtic@users.noreply.github.com>
2026-01-12 22:23:30 +0000

    Fix tenants app: Create models and initial migration

--- tenants/admin.py
+++ tenants/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django_tenants.admin import TenantAdminMixin
 
-# Register your models here.
+from tenants.models import School, Domain
+
+@admin.register(School)
+class SchoolAdmin(TenantAdminMixin, admin.ModelAdmin):
+    list_display = ('name', 'schema_name', 'created_on', 'on_trial', 'is_active')
+
+@admin.register(Domain)
+class DomainAdmin(TenantAdminMixin, admin.ModelAdmin):
+    list_display = ('domain', 'tenant', 'is_primary')
--- tenants/models.py
+++ tenants/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django_tenants.models import TenantMixin, DomainMixin
 
-# Create your models here.
+class School(TenantMixin):
+    name = models.CharField(max_length=100)
+    created_on = models.DateField(auto_now_add=True)
+    on_trial = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    
+    # Default true, schema will be automatically created and synced when it is saved
+    auto_create_schema = True
+
+    def __str__(self):
+        return self.name
+
+class Domain(DomainMixin):
+    pass

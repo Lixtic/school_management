e0312fb346336d5ffedba97ab21b55b9051ada82
Ray Gyapanin <72960660+Lixtic@users.noreply.github.com>
2026-01-12 19:24:19 +0000

    Import ProgrammingError in attendance view guard

--- students/views.py
+++ students/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.db.utils import ProgrammingError
 from django.http import JsonResponse, HttpResponse
 from django.db.models import Q, Count, Avg
 from datetime import date, timedelta
